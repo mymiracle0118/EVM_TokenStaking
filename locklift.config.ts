e2de4ad8f2fce6468e0e4f7201201ca1865463af
mymiracle0118 <noahsflood908@gmail.com>
2023-12-06 18:03:13 +0000

    nft part

--- locklift.config.ts
+++ locklift.config.ts
@@ -22,7 +22,7 @@ const config: LockliftConfig = {
     // path: "/mnt/o/projects/broxus/TON-Solidity-Compiler/build/solc/solc",
 
     // Or specify version of compiler
-    version: "0.64.0",
+    version: "0.61.2",
 
     // Specify config for extarnal contracts as in exapmple
     // externalContracts: {
@@ -36,7 +36,7 @@ const config: LockliftConfig = {
     // path: "/mnt/o/projects/broxus/TVM-linker/target/release/tvm_linker",
 
     // Or specify version of linker
-    version: "0.20.2",
+    version: "0.15.48",
   },
   networks: {
     locklift: {
--- scripts/2-deploy-staking.ts
+++ scripts/2-deploy-staking.ts
@@ -11,14 +11,14 @@ async function main() {
       publicKey: signer.publicKey,
       initParams: {
         _nonce: getRandomNonce(),
-        _owner: account.address,
+        _owner: account.address
       },
       constructorParams: {
         stakingTokenRoot: new Address('0:4e73ec103bc5c4998e7d92473fdd17ee7d4941fd681f07f6610085119a90ce1c'),
         stakingNFTRoot: new Address('0:127f28a512b73050a306a0836bb683bde3598e268594874b6aaa9a88c3b479d5'),
         sendRemainingGasTo: account.address
       },
-      value: locklift.utils.toNano(1.3),
+      value: locklift.utils.toNano(2),
     });
   
     console.log(`Staking deployed at: ${staking.address.toString()}`);
